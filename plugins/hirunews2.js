cdef8b6ec648349fdff34044dabe81e18f5d7b5d
mine-bbh <danupachithsara@gmail.com>
2024-09-26 21:18:43 +0530

    Update hirunews2.js

--- plugins/hirunews2.js
+++ plugins/hirunews2.js
@@ -1,10 +1,38 @@
-const config = require('../config')
-const { cmd, commands } = require('../command')
+const {cmd , commands} = require('../command')
+const { Buffer } = require('buffer');
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
+
+//=========News======
 cmd({
-    pattern: "hiru2",
-    alias: ["hnews2","hirunews2"],
-    react: "🗞️",
+    pattern: "derana",
+    alias: ["tvderana","derananews"],
+    react: "📡",
+    desc: "It gives derana news.",
+    category: "search",
+    use: '.derana',
+},
+async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+const data = await fetchJson('https://ada-derana-news-pink-venom.vercel.app')
+let info = `*📃 Title :* ${data.title}
+*⛓️ Link:* ${data.new_url}
+*📅 Time :* ${data.time}
+*📚 Description:* ${data.description}
+
+> *°⏰ [  𝗕𝗬  © Ｄａｒｋ < | | > Ｎｅｏｎ Ｃｙｂｅｒｚ ヤ • 🎉 ]*
+`
+return await conn.sendMessage(from, { image: { url: data.image} , caption: info } , { quoted: mek })
+} catch (e) {
+console.log(e)
+}
+})
+
+//=================================================================================================================
+
+cmd({
+    pattern: "hiru",
+    alias: ["hnews","hirunews"],
+    react: "🍎",
     desc: "It gives hiru news.",
     category: "search",
     use: '.hnews',
@@ -12,16 +40,15 @@ cmd({
 async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-const res = await fetchJson('https://vajira-api-site-65bfe70906e4.herokuapp.com/details/hirunews')
-let data = res.data
+const data = await fetchJson('https://app-97e3fc0d-9aec-4ff1-a518-b7b72a127d7c.cleverapps.io/api/latest')
 let info = `*📃 Title :* ${data.title}
-
+*🕒 Time:* ${data.time} 
+*⛓️ Id:* ${data.id}
 *📚 Description:* ${data.desc}
 
-
-${data.link}
+> *°⏰ [  𝗕𝗬  © Ｄａｒｋ < | | > Ｎｅｏｎ Ｃｙｂｅｒｚ ヤ • 🎉 ]*
 `
-return await conn.sendMessage(from, { image: { url: data.img} , caption: info } , { quoted: mek })
+return await conn.sendMessage(from, { image: { url: data.image} , caption: info } , { quoted: mek })
 } catch (e) {
 l(e)
 }
